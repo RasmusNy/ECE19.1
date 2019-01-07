2b603ac5e78f1f87571bc501b4f13c7a54ed07aa
RasmusNy <rasmus.nystrom@hotmail.com>
2019-01-07 15:25:10 -0800

    Update TestMain.m
    Reads in data file in matrix

--- TestMain.m
+++ TestMain.m
@@ -1,3 +1,39 @@
-disp('test');
-disp('test');
-disp('hello');
+%   Program process user file and stores data in appropriate matrix
+%   ECE19.1
+
+% Ask User For Data Typ AC/DC/Transient
+dataParameter = questdlg('What Data Parameter?', ...
+                'Data Format', ...
+                'AC', 'DC', 'Transient', 'Transient');
+
+% Handle Response
+switch dataParameter
+    case 'AC'
+        signalType = 0; % AC
+    case 'DC'
+        signalType = 1; % DC
+    case 'Transient'
+        signalType = 2; % Transient
+end
+
+% Select the file
+[fileName, pathName] = uigetfile('*.*','Pick Data File');
+dataFile = [pathName, fileName];
+
+%import data
+allData = importdata(dataFile);
+
+% Parse Parameter names
+%Time	C1 TRU OUTPUT V	C1 TRU OUTPUT I Pos	R TRU OUTPUT V	R TRU OUTPUT I
+%Pos%
+
+
+
+
+% AC File Processing
+
+% DC File Processing
+
+% Transient File Processing
+
+
