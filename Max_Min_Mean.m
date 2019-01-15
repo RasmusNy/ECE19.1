815b727a1235f3bca33f79336e1f2c3d43a182bf
huymvu <45314060+huymvu@users.noreply.github.com>
2019-01-15 15:48:13 -0800

    Add files via upload
    Max,Min,Mean function file

---
+++ Max_Min_Mean.m
@@ -0,0 +1,16 @@
+function [maxValue, minValue, meanValue] = Max_Min_Mean(DataMatrix) %DataMatrix = allData.data
+%DataMatrix = allData.data;      % get data from matrix
+VoltageData = DataMatrix(:,2);  % channel 2
+a = 5;  %get value at cursor position
+b = 10; %get value at cursor position
+if a<b 
+    maxValue = max(VoltageData(a:b));
+    minValue = min(VoltageData(a:b));
+    meanValue = mean(VoltageData(a:b));
+else
+    maxValue = max(VoltageData(b:a));
+    minValue = min(VoltageData(b:a));
+    meanValue = mean(VoltageData(a:b));
+end
+
+    
