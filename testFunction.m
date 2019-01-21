1cb2338af9d5e61b6083ca71c18e511711ed12ce
huymvu <45314060+huymvu@users.noreply.github.com>
2019-01-21 14:14:59 -0800

    Add files via upload

--- testFunction.m
+++ testFunction.m
@@ -1,2 +1,4 @@
 [maxValue, minValue, meanValue] = Max_Min_Mean(allData.data);
 [rmsValue] = RMS(allData.data);
+[crestfactorValue] = crestFactor(allData.data);
+%[upperEnvelope,lowerEnvelope] =envelope(allData.data(1:1:3000,2),15,'peak')
