58241473f0d8e989186431231024b808d0eab47f
huymvu <45314060+huymvu@users.noreply.github.com>
2019-01-22 12:18:02 -0800

    Update testFunction.m

--- testFunction.m
+++ testFunction.m
@@ -1,4 +1,6 @@
 [maxValue, minValue, meanValue] = Max_Min_Mean(allData.data);
 [rmsValue] = RMS(allData.data);
 [crestfactorValue] = crestFactor(allData.data);
-%[upperEnvelope,lowerEnvelope] =envelope(allData.data(1:1:3000,2),15,'peak')
+%[upperEnvelope,lowerEnvelope] =envelope(allData.data(1:1:3000,2),15,'peak');
+%[pks,locs] = findpeaks(upperEnvelope);
+%period = mean(diff(locs));
