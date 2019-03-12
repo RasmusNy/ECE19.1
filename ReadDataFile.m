c9e36514607912e64bae838a74e84ede857d72dd
huymvu <45314060+huymvu@users.noreply.github.com>
2019-03-12 15:12:13 -0700

    Update ReadDataFile.m

--- ReadDataFile.m
+++ ReadDataFile.m
@@ -52,18 +52,26 @@ cursor2 = 1.0000e-04;
 % cursor1 = -100;
 % cursor2 = 1.0123e-04;
 
+%calculate the distance between the time value from cursor and the time value from input data
 distance1 = abs(data(:,1) - cursor1);
 distance2 = abs(data(:,1) - cursor2);
 
+%if one cursor exceed the lower bound, get the index of minimum time value of input data
 if cursor1 < min(data(:,1)) && cursor2 <= max(data(:,1))
     cursorIndex1 = find(data(:,1)== min(data(:,1))); 
     cursorIndex2 = find(distance2 == min(distance2));
+    
+%if one cursor exceed the upper bound, get the index of maximum time value of input data
 elseif cursor2 > max(data(:,1)) && cursor1 >= min(data(:,1))
     cursorIndex1 = find(distance1 == min(distance1));
     cursorIndex2 = find(data(:,1)== max(data(:,1)));
+    
+%if both cursor exceed the input data range, get the index of minimum and maximum time value 
 elseif cursor1 < min(data(:,1)) && cursor2 > max(data(:,1))
     cursorIndex1 = find(data(:,1)== min(data(:,1))); 
     cursorIndex2 = find(data(:,1)== max(data(:,1)));
+    
+%if both cursor are in the input data range, get the index of the time values that are closest to the cursor position
 elseif cursor1 >= min(data(:,1)) && cursor2 <= max(data(:,1))
     cursorIndex1 = find(distance1 == min(distance1));
     cursorIndex2 = find(distance2 == min(distance2));
