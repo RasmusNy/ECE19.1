240f1c406505f1ef1995ede38b6c017ab27ae5bf
RasmusNy <rasmus.nystrom@hotmail.com>
2019-03-14 00:26:12 -0700

    Delete RMS.m

--- RMS.m
+++
@@ -1,9 +0,0 @@
-function [rmsValue] = RMS(DataMatrix)
-VoltageData = DataMatrix(:,2);  % channel 2, 
-a = 5;  %get value at cursor position
-b = 10; %get value at cursor position
-if a<b 
-    rmsValue = rms(VoltageData(a:b));
-else
-    rmsValue = rms(VoltageData(b:a));
-end
