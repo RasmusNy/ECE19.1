c9e862eeac706c97e8833f26abe6282bf47b9724
Jeremiah Kalmus <jeremiah.kalmus@gmail.com>
2019-04-07 19:52:03 -0700

    Add files via upload

--- ReadDataFile.m
+++ ReadDataFile.m
@@ -1,6 +1,7 @@
 %   Program process user file and stores data in appropriate matrix
 %   ECE19.1
-
+clear;
+clc;
 % Ask User For Data Typ AC/DC/Transient
 dataParameter = questdlg('What Data Parameter?', ...
                 'Data Format', ...
@@ -20,81 +21,177 @@ end
 [fileName, pathName] = uigetfile('*.*','Pick Data File');
 dataFile = [pathName, fileName];
 
-Fs = 100000;
+fs = 100000;
 %import data
 allData = importdata(dataFile);
 
-if(dataParameter == 'AC')
-    prompt = 'Enter fundamental frequency: ';
-    ffreq = input(prompt);
-end
-period = 1/ffreq;
+% if(dataParameter == 'AC')
+%     prompt = 'Enter Sampling frequency: ';
+%     fs = input(prompt);
+% end
+% period = 1/ffreq;
 %Maps matrix dimensions to m & n
 [m,n] = size(allData.data);
 
-%Plot allData
-% figure()
-% a = plot(allData.data(1:300,1),allData.data(1:300,2));
-% figure()
-% b = plot(allData.data(1:300,1),allData.data(1:300,5));
+vfft = fft(allData.data(:,2));
+ifft = fft(allData.data(:,5));
+n = length(allData.data(:,1));
+
+vabs = abs(vfft);
+iabs = abs(ifft);
+
+vabs_modified = vabs/1000;
+iabs_modified = iabs/1000;
+
+fvector = (allData.data(:,1) .* (fs/n));
+fvector((n/2)+2:end) = [];
+fvector = fvector .* 100000;
+
+phv = unwrap(angle(vfft));
+phi = unwrap(angle(ifft));
+
+iabs_modified((n/2)+2:end) = [];
+vabs_modified((n/2)+2:end) = [];
+
+%Graphing the fft of current and voltage
+figure(1)
+stem(fvector,iabs_modified);
+xlabel('f(Hz)'); ylabel('|I|'); title('|FFT| of i'); grid;
+figure(2)
+stem(fvector,vabs_modified);
+xlabel('f(Hz)'); ylabel('|V|'); title('|FFT| of v'); grid;
 
 
-% [up,lo] = envelope(allData.data(1:600:end,2));
-% hold on
-% plot(allData.data(1:600:end,1),up,allData.data(1:600:end,1),lo,'linewidth',1.5)
-% legend('voltage','up','lo')
-% hold off
-rmsv = rms(allData.data(:,2));
-rmsi = rms(allData.data(:,5));
-% Stot = rmsv * rmsi;
-% fun = allData.data(:,2) .* allData.data(:,5);
+pkv = [];
+locv = [];
+phv = [];
+vt = 0;
+vrms = [];
+[temp, val] = find(vabs_modified >= 0.1);
+locv = [locv ; val];
+phv = [phv ; angle(vfft(locv))];
+pkv = [pkv ; vabs_modified(locv)];
+locv = (locv - 1) ./ 2;
 
+pki = [];
+loci = [];
+phi = [];
+it = 0;
+irms = [];
+[temp, val] = find(iabs_modified >= 0.1);
+loci = [loci ; val];
+phi = [phi ; angle(ifft(loci))];
+pki = [pki ; iabs_modified(loci)];
+loci = (loci - 1) ./ 2;
 
-figure()
-thd(allData.data(:,2), Fs)
+Pavg = 0;
+Preact = 0;
+THDv = 0;
+THDi = 0;
+PF = 0;
+RMSv = [];
+RMSi = [];
 
-%Zero Crossing Data
-x = allData.data(1:300,1);               % Create Data
-y1 = allData.data(1:300,2);              % Create Data
-y2 = allData.data(1:300,5);              % Create Data
-ZC1 = ZeroX(x,y1);
-ZC2 = ZeroX(x,y2);
-
-%finding phase angle from zero crossing data
-x_difference = ZC1(1) - ZC2(1);
-percent_diff = x_difference/period;
-phase_angle = percent_diff * 2* pi;
-
-%power calculations
-power_factor = cos(phase_angle);
-real_power = rmsv * rmsi * cos(phase_angle);
-reactive_power = rmsv * rmsi * sin(phase_angle);
-
-%Plot Channel 2 zero crossings
-figure()
-plot(x, y1, '-r')
-hold on
-plot(ZC1, zeros(size(ZC1)), 'pg')
-hold off
-grid
-
-%plot channel 5 zero crossings
-figure()
-plot(x, y2, '-r')
-hold on
-plot(ZC2, zeros(size(ZC2)), 'pg')
-hold off
-grid
-
-
-
-%Calculate Max, Min, Mean
-maximum = max(allData.data(:,2));
-
-minimum = min(allData.data(:,2));
-
-% Parse Parameter names
-%Time	C1 TRU OUTPUT V	C1 TRU OUTPUT I Pos	R TRU OUTPUT V	R TRU OUTPUT I
-% function output = multiply(allData.data(:,2),allData.data(:,5))
-% output = allData.data(:,2) .* allData.data(:,5);
+% % Voltage as a function of time
+% for k = 1:length(phv)
+%     temp = pkv(k)*sin(k*w*t+phv(k));
+%     vt = (vt + temp);
 % end
+% 
+% % Current as a function of time
+% for k = 1:length(phi)
+%     temp = pki(k)*sin(k*w*t+phi(k));
+%     it = (it + temp);
+% end
+
+%Voltage rms
+for k = 1:length(phv)
+    temp = sqrt((pkv(k)^2)/2);
+    vrms = vrms + temp;
+    RMSv = [RMSv ; temp];
+end
+
+% Current rms
+for k = 1:length(phi)
+    temp = sqrt((pki(k)^2)/2);
+    irms = irms + temp;
+    RMSi = [RMSi ; temp];
+end
+counter = 1;
+% Real Power
+if(length(phv) >= length(phi))
+    counter = 1;
+    while counter ~= length(loci)
+        for k = 1:length(locv)
+            if loci(counter) == locv(k)
+               temp = RMSv(k)*RMSi(k)*(cos(phv(k)-phi(k)));
+               Pavg = Pavg + temp;
+            end
+        end
+        counter = counter + 1;
+    end
+elseif(length(phv) < length(phi))
+    while counter ~= length(loci)
+        for k = 1:length(locv)
+            if loci(counter) == locv(k)
+               temp = RMSv(k)*RMSi(k)*(cos(phv(k)-phi(k)));
+               Pavg = Pavg + temp;
+            end
+        end
+        counter = counter + 1;
+    end
+end
+counter = 1;
+% Reactive power
+if(length(phv) >= length(phi))
+    while counter ~= length(loci)
+        for k = 1:length(locv)
+            if loci(counter) == locv(k)
+                temp = RMSv(k)*RMSi(k)*(sin(phv(k)-phi(k)));
+                Preact = Preact + temp;
+            end
+        end
+        counter = counter + 1;
+    end
+elseif(length(phv) < length(phi))
+    while counter ~= length(loci)
+        for k = 1:length(locv)
+            if loci(counter) == locv(k)
+                temp = RMSv(k)*RMSi(k)*(sin(phv(k)-phi(k)));
+                Preact = Preact + temp;
+            end
+        end
+        counter = counter + 1;
+    end
+end
+
+% % THD for Voltage
+% for k = 2:length(phv)
+%    temp = (sqrt(vrms(k)^2)/vrms(1));
+%    THDv = THDv + temp;
+% end
+% THDv = THDv * 100;
+% 
+% % THD for Current
+% for k = 2:length(phi)
+%    temp = (sqrt(irms(k)^2)/irms(1));
+%    THDi = THDi + temp;
+% end
+% THDi = THDi * 100;
+% 
+% RMSv = vrms(1)*(sqrt(1+(THDv/100)^2));
+% RMSi = irms(1)*(sqrt(1+(THDi/100)^2));
+
+%Power Factor
+if(length(phv) >= length(phi))
+    for k = 1:length(phi)
+       PF = (Pavg/(irms*vrms));
+    end
+elseif(length(phv) < length(phi))
+    for k = 1:length(phv)
+       PF = (Pavg/(irms*vrms));
+%      temp = (Pavg/(vrms(1)*irms(1)))*(1/((sqrt(1+(THDv/100)^2))*(sqrt(1+(THDi/100)^2))));
+%        PF = PF + temp;
+    end
+end
+
--- TestPQ.m
+++ TestPQ.m
@@ -3,9 +3,11 @@ clear;
 clc;
 w=pi;
 t=[0:.001:2];
+%v=1*cos(w*t) + .1*cos(10*w*t);%voltage
 
+i=.8*cos(w*t + 10*pi/180) + 0.25*cos(2*w*t + 15*pi/180) + 0.5*cos(3*w*t + 19*pi/180) + .125*cos(6*w*t+35*pi/180);%current
 v=1*cos(w*t);%voltage
-i=.8*cos(w*t) + 0.25*cos(2*w*t + 15*pi/180) + 0.5*cos(3*w*t + 19*pi/180) + .125*cos(6*w*t+35*pi/180);%current
+
 f = w/(2*pi);
 fs = 1/0.001;
 
@@ -19,7 +21,11 @@ iabs = abs(ifft);
 vabs_modified = vabs/1000;
 iabs_modified = iabs/1000;
 
-fvector=[0:0.5:fs/2];
+% fvector=[0:0.5:fs/2];
+fvector = (t .* (fs/n));
+fvector((n/2)+1:end) = [];
+fvector = fvector .* fs;
+%fvector(fvector/2:end) = [];
 
 phv = unwrap(angle(vfft));
 phi = unwrap(angle(ifft));
@@ -34,13 +40,16 @@ xlabel('f(Hz)'); ylabel('|I|'); title('|FFT| of i'); grid;
 figure(2)
 stem(fvector,vabs_modified);
 xlabel('f(Hz)'); ylabel('|V|'); title('|FFT| of v'); grid;
-
+figure(3)
+plot(t,v);
+figure(4)
+plot(t,i);
 
 pkv = [];
 locv = [];
 phv = [];
 vt = 0;
-vrms = [];
+vrms = 0;
 [temp, val] = find(vabs_modified >= 0.1);
 locv = [locv ; val];
 phv = [phv ; angle(vfft(locv))];
@@ -51,7 +60,7 @@ pki = [];
 loci = [];
 phi = [];
 it = 0;
-irms = [];
+irms = 0;
 [temp, val] = find(iabs_modified >= 0.1);
 loci = [loci ; val];
 phi = [phi ; angle(ifft(loci))];
@@ -59,9 +68,12 @@ pki = [pki ; iabs_modified(loci)];
 loci = (loci - 1) ./ 2;
 
 Pavg = 0;
+Preact = 0;
 THDv = 0;
 THDi = 0;
 PF = 0;
+RMSv = [];
+RMSi = [];
 
 % Voltage as a function of time
 for k = 1:length(phv)
@@ -78,51 +90,96 @@ end
 %Voltage rms
 for k = 1:length(phv)
     temp = sqrt((pkv(k)^2)/2);
-    vrms = [vrms ; temp];
+    vrms = vrms + temp;
+    RMSv = [RMSv ; temp];
 end
 
 % Current rms
 for k = 1:length(phi)
     temp = sqrt((pki(k)^2)/2);
-    irms = [irms ; temp];
+    irms = irms + temp;
+    RMSi = [RMSi ; temp];
 end
+counter = 1;
 
 % Real Power
 if(length(phv) >= length(phi))
+    counter = 1;
     for k = 1:length(phi)
-       temp = vrms(k)*irms(k)*(cos(phv(k)-phi(k)));
-       Pavg = Pavg + temp;
+        while k ~= length(loci)
+            if loci(counter) == locv(k)
+                temp = RMSv(k)*RMSi(k)*(cos(phv(k)-phi(k)));
+                Pavg = Pavg + temp;
+            end
+        end
     end
 elseif(length(phv) < length(phi))
-    for k = 1:length(phv)
-       temp = vrms(k)*irms(k)*(cos(phv(k)-phi(k)));
-       Pavg = Pavg + temp;
+    while counter ~= length(loci)
+        for k = 1:length(locv)
+            if loci(counter) == locv(k)
+               temp = RMSv(k)*RMSi(k)*(cos(phv(k)-phi(k)));
+               Pavg = Pavg + temp;
+            end
+        end
+        counter = counter + 1;
+    end
+end
+icount = 1;
+vcount = 1;
+% Reactive power
+if(length(phv) >= length(phi))
+    for k = 1:length(phi)
+       temp = RMSv(k)*RMSi(k)*(sin(phv(k)-phi(k)));
+       Preact = Preact + temp;
+    end
+elseif(length(phv) < length(phi))
+    while icount ~= length(loci) && vcount ~= length(locv)
+       if icount == length(loci) && loci(icount) < locv(vcount)
+           vcount = length(vcount);
+       elseif vcount == length(locv) && loci(icount) > locv(vcount)
+           icount = length(icount);
+       end
+        
+       if loci(icount) > locv(vcount)
+           vcount = vcount + 1;
+       elseif loci(icount) < locv(vcount)
+           icount = icount + 1;
+       else
+           temp = RMSv(vcount)*RMSi(icount)*(sin(phv(vcount)-phi(icount)));
+           Preact = Preact + temp;
+           icount = icount + 1;
+           vcount = vcount + 1;
+       end
     end
 end
 
-% THD for Voltage
-for k = 2:length(phv)
-   temp = (sqrt(vrms(k)^2)/vrms(1));
-   THDv = THDv + temp;
-end
-THDv = THDv * 100;
-
-% THD for Currrent
-for k = 2:length(phi)
-   temp = (sqrt(irms(k)^2)/irms(1));
-   THDi = THDi + temp;
-end
-THDi = THDi * 100;
+% % THD for Voltage
+% for k = 2:length(phv)
+%    temp = (sqrt(vrms(k)^2)/vrms(1));
+%    THDv = THDv + temp;
+% end
+% THDv = THDv * 100;
+% 
+% % THD for Current
+% for k = 2:length(phi)
+%    temp = (sqrt(irms(k)^2)/irms(1));
+%    THDi = THDi + temp;
+% end
+% THDi = THDi * 100;
+% 
+% RMSv = vrms(1)*(sqrt(1+(THDv/100)^2));
+% RMSi = irms(1)*(sqrt(1+(THDi/100)^2));
 
 %Power Factor
 if(length(phv) >= length(phi))
     for k = 1:length(phi)
-       temp = Pavg/(vrms(k)*irms(k));
+       temp = (Pavg/(RMSv*RMSi));
        PF = PF + temp;
     end
 elseif(length(phv) < length(phi))
     for k = 1:length(phv)
-       temp = Pavg/(vrms(k)*irms(k));
-       PF = PF + temp;
+       PF = (Pavg/(irms*vrms));
+%      temp = (Pavg/(vrms(1)*irms(1)))*(1/((sqrt(1+(THDv/100)^2))*(sqrt(1+(THDi/100)^2))));
+%        PF = PF + temp;
     end
 end
