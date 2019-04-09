a6a2272da4a147d139f210354f2d9ae7a7952abf
huymvu <45314060+huymvu@users.noreply.github.com>
2019-04-09 14:54:51 -0700

    Add files via upload

--- modulation.m
+++ modulation.m
@@ -1,9 +1,10 @@
 %Modulation
 %Input signal
+load('allData.mat')
 Data = allData.data(:,2);
 t = allData.data(:,1);
 a = 1;
-b = 5000;
+b = 10000;
 
 if a < b
     [upperEnvelope,lowerEnvelope] = envelope(Data(a:b),150,'peak');
@@ -18,3 +19,31 @@ elseif a > b
     modulationAmplidute = maxValue - minValue;
 end
 envelope(Data(a:b),150,'peak')
+% dt = t(2)-t(1);
+% findpeaks(Data(a:b),'MinPeakHeight',31.2300,'MinPeakDistance',round(0.5/100000))
+% findpeaks(Data(a:b),'MinPeakHeight',31.2300)
+% envelope(Data(a:b),round(0.5/100000),'peak')
+
+% signal = allData.data(1:1:1000,2);
+% t = linspace(1,stopIndex,length(M));
+% 
+% peaks = findpeak(signal);
+% maxPeak = max(peaks);
+% minPeak = min(peaks);
+% 
+% %Return difference between maximum peak and minimum peak
+% M = maxPeak - minPeak 
+% 
+% 
+% plot(t,signal)
+% 
+% [up,lo] = envelope(y);
+% hold on
+% plot(t,up,t,lo')
+% hold off
+% 
+% title('Modulation')
+% ylabel('Amplitude')
+% xlabel('# of Data Points')
+% return
+% end
